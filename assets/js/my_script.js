f948fe96813a3b0ceccd77a67c98959a46ed393d
Pedro Cristo <pcristo35@gmail.com>
2022-06-18 13:17:25 +0000

    Add script for preloader

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -1,40 +1,42 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
             $('.navbar .logo a').css("font-size", "25px");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
             $('.navbar .logo a').css("font-size", "35px");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
             $('.scroll-up-btn').css("margin-right", "5px").css("top", "580px");
-            
-        }else{
+
+        } else {
             $('.scroll-up-btn').removeClass("show");
             $('.scroll-up-btn').css("margin-right", "-1px").css("top", "580px");
-            
+
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -55,7 +57,7 @@ $(document).ready(function(){
         loop: true
     });
 
-  
+
 
     // owl carousel script
     $('.carousel').owlCarousel({
@@ -65,15 +67,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
@@ -83,47 +85,58 @@ $(document).ready(function(){
 
 //PARALLAX
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     parallax();
-    })
-    function parallax(){
-    
+})
+
+function parallax() {
+
     var wScroll = $(window).scrollTop();
-    
-    
+
+
     $(".parallax").css("background-position",
-    "center "+(wScroll*0.60)+"px")
-        
+        "center " + (wScroll * 0.60) + "px")
+
     $(".parallax-2").css("background-position",
-    "center "+(wScroll*2)+"px")
+        "center " + (wScroll * 2) + "px")
 
     $(".parallax-box").css("top",
-    -50+(wScroll*0.50)+"em")
-    
+        -50 + (wScroll * 0.50) + "em")
+
+}
+
+// RECAPTCHA RESPONSIVE
+
+$(function () {
+    function rescaleCaptcha() {
+        var width = $('.g-recaptcha').parent().width();
+        var scale;
+        if (width < 302) {
+            scale = width / 302;
+        } else {
+            scale = 1.0;
+        }
+
+        $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('transform-origin', '0 0');
+        $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
     }
 
-    // RECAPTCHA RESPONSIVE
-
-    $(function(){
-        function rescaleCaptcha(){
-          var width = $('.g-recaptcha').parent().width();
-          var scale;
-          if (width < 302) {
-            scale = width / 302;
-          } else{
-            scale = 1.0; 
-          }
-      
-          $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
-          $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
-          $('.g-recaptcha').css('transform-origin', '0 0');
-          $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
-        }
-      
+    rescaleCaptcha();
+    $(window).resize(function () {
         rescaleCaptcha();
-        $( window ).resize(function() { rescaleCaptcha(); });
-      
-      });
+    });
 
-      // SHOW CORRENT YEAR IN THE FOOTER
-      $("#corrent-year").text(new Date().getFullYear());
+});
+
+// SHOW CORRENT YEAR IN THE FOOTER
+$("#corrent-year").text(new Date().getFullYear());
+
+//PRELOADER
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+
+    loader.style.display = "none";
+})
