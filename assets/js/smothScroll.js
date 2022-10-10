61fdf689c538f764cb8871f02b6092f1fa3378e6
Pedro Cristo <pcristo35@gmail.com>
2022-10-10 13:29:49 +0000

    Add jquery script for smooth scroll

---
+++ assets/js/smothScroll.js
@@ -0,0 +1,14 @@
+ // Smooth scroll on click
+ $("a").on('click', function (e) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      e.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000);
+    }
+    // End if
+  });
