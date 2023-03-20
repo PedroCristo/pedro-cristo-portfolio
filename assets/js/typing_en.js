6ffe8a990eb77df07d8a43516147b230ccface13
Pedro Cristo <pcristo35@gmail.com>
2023-03-20 15:30:21 +0000

    Add pages translated in portuguese

---
+++ assets/js/typing_en.js
@@ -0,0 +1,14 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
---
+++ assets/js/typing_pt.js
@@ -0,0 +1,15 @@
+    // typing text animation script PT
+    var typed = new Typed(".typing", {
+        strings: ["Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Desenvolvedor", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
