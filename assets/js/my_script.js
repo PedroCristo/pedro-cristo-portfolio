edca4dc9c5aa79a9b8ab0eeee4f13080f3b01169
Pedro Cristo <pcristo35@gmail.com>
2023-06-03 12:16:26 +0000

    Invert direction for carousel OWL

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -45,27 +45,28 @@ $(document).ready(function () {
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
+    $('.carousel').each(function(index) {
+        const rtlConfig = index === 0 ? { rtl: true } : {};
+        $(this).owlCarousel({
+          margin: 20,
+          loop: true,
+          autoplay: true,
+          autoplayTimeout: 2000,
+          autoplayHoverPause: true,
+          responsive: {
             0: {
-                items: 1,
-                nav: false
+              items: 1
             },
             600: {
-                items: 2,
-                nav: false
+              items: 2
             },
             1000: {
-                items: 3,
-                nav: false
+              items: 3
             }
-        }
-    });
+          },
+          ...rtlConfig // Spread operator to merge rtlConfig object
+        });
+      });
 });
 
 //PARALLAX
